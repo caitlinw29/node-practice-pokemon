7e9d8fa96e135da28a09d3b93615179bf63e66f3
Caitlin Wall <caitlinwall96@gmail.com>
2022-01-14 17:33:41 -0500

    Rearrange flow so that generateHTML is called AFTER the fetch, providing access to more data

--- index.js
+++ index.js
@@ -1,11 +1,7 @@
-//add libraries
-// switch to imports for node-fetch
 import inquirer from "inquirer";
 import fs from 'fs';
 import generateHTML from './utils/generateHTML.js';
 import fetch from "node-fetch";
-import download from 'image-downloader';
-
 
 //Set up function to write answers in file
 function writeToFile(fileName, data) {
@@ -14,7 +10,7 @@ function writeToFile(fileName, data) {
     );
  }
 
-//inquire the questions, then put answers in a writeToFile function aimed at the index.html
+//inquire asks the question, then put answer in a writeToFile function aimed at the index.html
 function init() {
     inquirer
       .prompt([     
@@ -25,8 +21,6 @@ function init() {
         }
       ])
       .then((data) => {
-            //pass the data into the html js file that was imported 
-            writeToFile("index.html", generateHTML({...data})); 
             //lowercase for url   
             const pokeName = data.pokemon.toLowerCase(); 
             const pokeAPI = "https://pokeapi.co/api/v2/pokemon/" + pokeName;
@@ -38,19 +32,10 @@ function init() {
                 return response.json();
               })
               .then(function (data) {
-                let pokeImage = data.sprites.front_default;
-                
-                const options = {
-                  url: pokeImage,
-                  dest: './assets/img/pokeImage.png'     // will be saved as pokeImage.png
-                }
-
-                download.image(options)
-                  .then(({ filename }) => {
-                    console.log('Saved to', filename)  // saved to /path/to/dest/photo
-                  })
-                  .catch((err) => console.error(err))
-
+                //switch back to uppercase for displaying the name
+                const pokemon = pokeName[0].toUpperCase() + pokeName.substr(1); 
+                //pass the data into the html js file that was imported
+                writeToFile("index.html", generateHTML({...data}, pokemon)); 
               })
       })
 }
