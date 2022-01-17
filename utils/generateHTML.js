b9b9d3980ef8ede724bb38a9d1f5d70b8fb86445
Caitlin Wall <caitlinwall96@gmail.com>
2022-01-16 19:07:01 -0500

    Update html/css to create pokedex layout

--- utils/generateHTML.js
+++ utils/generateHTML.js
@@ -9,12 +9,40 @@ function generateHTML(data, pokemon) {
       <meta name="description" content="Pokemon page generated with node.js from command line">
       <meta name="viewport" content="width=device-width, initial-scale=1">
       <link rel="icon" type="image/jpg" href="./assets/img/poke-logo.jpg">
-      <link rel="stylesheet" href="node_modules/bootstrap/dist/css/bootstrap.min.css">
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
     </head>
-    <body class="container text-center">
-      <h1 class="display-1">${pokemon}</h1>
-      <p>${data.order}</p>
-      <img src="${data.sprites.front_default}" class="img-fluid" alt="Your pokemon" id="pokeImage">
+    <body class="container center-align yellow lighten-3">
+      <div class="row valign-wrapper">
+        <div class="red col s2 white-text"><p>#${data.id}</p></div>
+        <h1 class="col s8">${pokemon}</h1>
+        <div class="col s2"></div>
+      </div>
+      <div class="row valign-wraper">
+        <img src="${data.sprites.front_default}" class="col s4 offset-s4" alt="${pokemon}">
+      </div>
+      <div class="row">
+        <div class="col s8 offset-s2 red lighten-2">
+          <p>Type: ${data.types[0].type.name}</p>
+          <p>Height: ${data.height}</p>
+          <p>Weight: ${data.weight}</p>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col s4 offset-s2">
+          <a class="btn-large red">${data.moves[0].move.name}</a>
+        </div>
+        <div class="col s4">
+          <a class="btn-large red">${data.moves[1].move.name}</a>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col s4 offset-s2">
+          <a class="btn-large red">${data.moves[2].move.name}</a>
+        </div>
+        <div class="col s4">
+          <a class="btn-large red">${data.moves[3].move.name}</a>
+        </div>
+      </div>
     </body>
   </html>
 `;
